83d306c4004cd0477e9213d206654c2b1df15eb3
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-07-24 14:05:46 -0300

    -q

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+<!-- <div class="menu">
+  <router-link to="/">Home</router-link>
+</div> -->
+<header>
+    <div class="header container center">
+      <div class="header-text">
+        <h1>Crafting beautiful UI/UX into seamless semantic code.</h1>
+        <p>Located in the old English town of Shrewsbury, home to the world's first skyscraper & the oldest working brewery. Lead Designer at Zengenti</p>
+        <button type="button" name="button">More about me</button>
+        <img src="" alt="">
+      </div>
+    </div>
+</header>
+
+
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+   @import './src/assets/css/main.scss';
+  header {
+    width: 100vw;
+    height: 100vh;
+    background: #161616;
+    background: -webkit-linear-gradient(120deg, #161616, #2B2836); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(120deg, #161616, #2B2836); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+      .header {
+        width: 100%;
+        height: 100%;
+
+        .header-text {
+          width: 800px;
+          max-width: 90%;
+          color: #fff;
+            h1 {
+              font-size: 3.5em;
+              line-height: 110%;
+              padding-bottom: 20px
+            }
+            p {
+              font-size: 1.5em;
+              padding-bottom: 20px;
+            }
+            button {
+              background: $primary;
+              line-height: 50px;
+              height: 50px;
+              border: none;
+              width: 250px;
+              color: #fff;
+              font-size: 1.2em;
+              font-weight: 900;
+              border-radius: 4px;
+            }
+        }
+      }
+  }
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="hello">
+    <div class="content size margin ">
+      <div class="title">
+        <h1>Powering Your Favorites</h1>
+        <p>From e-Commerce, travel, and social media, to consumer brands and business services.</p>
+      </div>
+
+
+      <div class="container-draw">
+        <div class="draw-img">
+          <div class="container-draw-img">
+            <div class="work-img" v-for="work in justDrawOne">
+              <img :src="require('@/assets/images/draws/' + work.image)" width="100%">
+            </div>
+
+          </div>
+        </div>
+        <div class="draw-img">
+          <div class="container-draw-img">
+            <div class="work-img" v-for="work in justDrawTwo">
+              <img :src="require('@/assets/images/draws/' + work.image)" width="100%">
+            </div>
+
+          </div>
+        </div>
+        <div class="draw-img">
+          <div class="container-draw-img">
+            <div class="work-img" v-for="work in justDrawThree">
+              <img :src="require('@/assets/images/draws/' + work.image)" width="100%">
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+    <!-- <ul>
+  <li v-for="work in works">{{work.title}}</li>
+</ul> -->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    computed: {
+    justDrawOne(){
+      return this.works.filter(works => works.tag == '1');
+    },
+    justDrawTwo(){
+      return this.works.filter(works => works.tag == '2');
+    },
+    justDrawThree(){
+      return this.works.filter(works => works.tag == '3');
+    },
+  },
+    data() {
+      return {
+        works: [{
+          title: 'title 1',
+          type: 'draw',
+          tag: '1',
+          image: '1.png'
+        },
+        {
+          title: 'title 11',
+          type: 'draw',
+          tag: '2',
+          image: '11.jpg'
+        },
+        {
+          title: 'title 2',
+          type: 'draw',
+          tag: '1',
+          image: '2.png'
+        }, {
+          title: 'title 3',
+          type: 'draw',
+          tag: '3',
+          image: '3.png'
+        },
+        {
+          title: 'title 4',
+          type: 'draw',
+          tag: '1',
+          image: '4.jpg'
+        },
+        {
+          title: 'title 5',
+          type: 'draw',
+          tag: '2',
+          image: '5.png'
+        },
+        {
+          title: 'title 6',
+          type: 'draw',
+          tag: '2',
+          image: '6.jpg'
+        },
+        {
+          title: 'title 7',
+          type: 'draw',
+          tag: '3',
+          image: '7.jpg'
+        },
+        {
+          title: 'title 8',
+          type: 'draw',
+          tag: '3',
+          image: '8.jpg'
+        },
+        {
+          title: 'title 9',
+          type: 'draw',
+          tag: '3',
+          image: '9.png'
+        },
+        {
+          title: 'title 10',
+          type: 'draw',
+          tag: '1',
+          image: '10.jpg'
+        },
+
+        {
+          title: 'title 12',
+          type: 'draw',
+          tag: '1',
+          image: '31.png'
+        },
+        {
+          title: 'title 13',
+          type: 'draw',
+          tag: '2',
+          image: '32.png'
+        },
+      ],
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .container-draw {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    .draw-img {
+      min-height: 100px;
+      margin: 1px;
+      flex-grow: 1;
+      flex-basis: 300px
+    }
+  }
+
+  .container-draw-img {
+    display: flex;
+    flex-direction: column;
+    .work-img {
+      flex-grow: 1;
+      margin: 10px;
+      cursor: pointer;
+        img {
+          border: 1px solid #e8eaeb
+        }
+    }
+  }
+
+  @media screen and (max-width: 480px) {
+    .container-draw {
+      flex-wrap: wrap;
+    }
+  }
+  .title {
+    padding-top: 30px;
+    padding-bottom: 20px;
+  }
+</style>
